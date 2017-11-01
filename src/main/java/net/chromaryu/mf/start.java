7815bb7dd6482b06f10d07ed0e0c1e37e7a1b647
Chromaryu <knight.ryu12@gmail.com>
2017-11-02 01:08:04 +0900

    changed message

--- src/main/java/net/chromaryu/mf/start.java
+++ src/main/java/net/chromaryu/mf/start.java
@@ -2,6 +2,6 @@ package net.chromaryu.mf;
 
 public class start {
     public static void main(String[] args) {
-        System.out.println("Hello TeamCity CI");
+        System.out.println("Hello TeamCity CI, Also world too :D");
     }
 }
